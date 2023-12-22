47f6f7775cd401b817529816d4c97561f4af8a1a
Juampi Badino <juanpablobadino6@gmail.com>
2023-12-22 16:29:07 -0300

    Versión de escritorio

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home.tsx';
+//import AboutUs from './pages/AboutUs.tsx';
+//import WhatWeDo from './pages/WhatWeDo.tsx';
+//import RaceWithUs from './pages/RaceWithUs.tsx';
+//import CaseStudies from './pages/CaseStudies.tsx';
+//import Events from './pages/Events.tsx';
+//import DecemberHackJam from './pages/Events/DecemberHackJam.tsx';
+//import Contact from './pages/Contact.tsx';
+//import World from './pages/World.tsx';
+//import BarqiaSportsWorld from './pages/BarqiaSportsWorld.tsx';
+
+function App() {
+  return (
+    <div className="App" style={{background: 'black'}}>
+      <Routes>
+          <Route path='' index element={<Home />} />
+          {/**
+          <Route path='AboutUs' element={<AboutUs />} />
+          <Route path='WhatWeDo' element={<WhatWeDo />} />
+          <Route path='RaceWithUs' element={<RaceWithUs />} />
+          <Route path='CaseStudies' element={<CaseStudies />} />
+          <Route path='Events' element={<Events />} />
+          <Route path='Events/DecemberHackJam' element={<DecemberHackJam />} />
+          <Route path='Contact' element={<Contact />} />
+          <Route path='World' element={<World />} />
+          <Route path='BarqiaSportsWorld' element={<BarqiaSportsWorld />} />
+           */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import './Home.css';
+
+import { Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
+const Home = () => (
+    <Container id='home-container'>
+        <img src='/img/navbar_laser.png' />
+        <div id='nav-bar'>
+            <a href=''>what we do</a>
+            <a href=''>about us</a>
+            <a href=''>case studies</a>
+            <a href=''>contact</a>
+        </div>
+        <img src='/img/Logo_inicio_Reflejo.png' style={{ marginLeft: '40px' }} />
+        <h1 style={{ background: '#171618', color: 'white', textAlign: 'center' }} >
+            WELCOME TO
+        </h1>
+        <video autoPlay muted loop style={{ width: '100%' }} >
+            <source src="video/Video_BarqIA_Promo.mp4" type="video/mp4" />
+        </video>
+        <Container style={{ position: 'relative' }}>
+            <img src='/img/poligonos.png' style={{ position: 'absolute', top: '0', width: '100%', zIndex: '0' }} />
+            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+                <img src='/img/barqia-symbol-gif.gif' style={{ marginTop: '-100px' }} />
+            </div>
+            <p id='feel-the-passion' style={{ marginTop: '-100px', position: 'relative', zIndex: '1' }}>
+                Feel the passion for sports in a more immersive way. Interact with your sports entity and move up levels where you can grow your club and your passion. Live the emotion with Barqia Sports. <br />
+                We are passionate about sports and technology. Barqia Sports wants to deliver you a more interactive and technological way to continue living sports to spread the same passion to you.
+            </p>
+            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+                <img src='/img/home-1.PNG' />
+            </div>
+            <p style={{ position: 'relative', zIndex: '1' }}>
+                The immersive platform that offers fans the opportunity to create a closer bond with athletes and sports entities. Offer innovative and technological services to your fans and they will return it to you.
+            </p>
+            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+                <img src='/img/home-2.PNG' />
+            </div>
+            <p style={{ position: 'relative', zIndex: '1' }}>
+                The immersive platform that offers fans the opportunity to create a closer bond with athletes and sports entities. Offer innovative and technological services to your fans and they will return it to you.
+            </p>
+            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+                <img src='/img/home-3.PNG' />
+            </div>
+        </Container>
+        
+
+
+        <Container id='footer' style={{ background: 'black', border: '1px solid rgba(256, 256, 256, 0.2)', borderRadius: '10px', padding: '50px', position: 'relative', zIndex: '2' }}>
+            <Row>
+                <Col style={{ textAlign: 'center' }}>
+                    <img className='hide-on-mobile' src='/img/barqia-symbol.png' style={{ width: '75px' }} />
+                </Col>
+            </Row>
+            <hr style={{ margin: '40px 0' }} />
+            <Row id='barqia-social-networks'>
+                <Col style={{ textAlign: 'center' }}>
+                    <a target="_blank" href='https://www.youtube.com/@barqiasports'><img src='/img/icon-youtube.png' style={{ filter: 'brightness(0) invert(1)', width: '37px' }} /></a>
+                    <a target="_blank" href='https://www.linkedin.com/company/barqiasports/?originalSubdomain=es'><img src='/img/icon-linkedin.png' style={{ filter: 'brightness(0) invert(1)', margin: '0 40px', marginTop: '-10px', width: '37px' }} /></a>
+                    <a target="_blank" href='https://www.instagram.com/barqiasports/'><img src='/img/icon-instagram.png' style={{ filter: 'brightness(0) invert(1)', width: '37px' }} /></a>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p style={{ color: 'white', fontSize: '15px', marginTop: '45px', textAlign: 'center' }}>
+                        2023 Barqia - All Rights Reserved.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    </Container>
+)
+
+export default Home;
