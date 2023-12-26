5d6b46a856bb9339adc6f0c7dbbb20561c55fd51
Juampi Badino <juanpablobadino6@gmail.com>
2023-12-26 08:11:03 -0300

    Adaptación mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,52 +2,55 @@ import React from 'react';
 
 import './Home.css';
 
-import { Row, Col } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { Row, Col } from 'react-bootstrap';
 
 const Home = () => (
     <Container id='home-container'>
-        <img src='/img/navbar_laser.png' />
+        <img id='navbar-laser' src='/img/navbar_laser.png' />
         <div id='nav-bar'>
-            <a href=''>what we do</a>
-            <a href=''>about us</a>
-            <a href=''>case studies</a>
-            <a href=''>contact</a>
+            <div style={{ display: 'inline-block' }}>
+                <img src='/img/barqia-symbol.png' />
+            </div>
+            <div style={{ float: 'right' }}>
+                <a href=''>what we do</a>
+                <a href=''>about us</a>
+                <a href=''>case studies</a>
+                <a href=''>contact</a>
+            </div>
         </div>
-        <img src='/img/Logo_Inicio_Reflejo.png' style={{ marginLeft: '40px' }} />
+        <img id='logo-inicio-reflejo' src='/img/Logo_Inicio_Reflejo.png' style={{ marginLeft: '40px' }} />
         <h1 style={{ background: '#171618', color: 'white', textAlign: 'center' }} >
-            WELCOME TO
+            WELCOME TO BARQIA
         </h1>
         <video autoPlay muted loop style={{ width: '100%' }} >
             <source src="video/Video_BarqIA_Promo.mp4" type="video/mp4" />
         </video>
         <Container style={{ position: 'relative' }}>
             <img src='/img/poligonos.png' style={{ position: 'absolute', top: '0', width: '100%', zIndex: '0' }} />
-            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+            <div id='barqia-symbol-gif-container' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
                 <img src='/img/barqia-symbol-gif.gif' style={{ marginTop: '-100px' }} />
             </div>
-            <p id='feel-the-passion' style={{ marginTop: '-100px', position: 'relative', zIndex: '1' }}>
+            <p style={{ position: 'relative', zIndex: '1' }}>
                 Feel the passion for sports in a more immersive way. Interact with your sports entity and move up levels where you can grow your club and your passion. Live the emotion with Barqia Sports. <br />
                 We are passionate about sports and technology. Barqia Sports wants to deliver you a more interactive and technological way to continue living sports to spread the same passion to you.
             </p>
-            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+            <div style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
                 <img src='/img/home-1.PNG' />
             </div>
             <p style={{ position: 'relative', zIndex: '1' }}>
                 The immersive platform that offers fans the opportunity to create a closer bond with athletes and sports entities. Offer innovative and technological services to your fans and they will return it to you.
             </p>
-            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+            <div style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
                 <img src='/img/home-2.PNG' />
             </div>
             <p style={{ position: 'relative', zIndex: '1' }}>
                 The immersive platform that offers fans the opportunity to create a closer bond with athletes and sports entities. Offer innovative and technological services to your fans and they will return it to you.
             </p>
-            <div id='the-immersive-platform' style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
+            <div style={{ textAlign: 'center', position: 'relative', zIndex: '1' }}>
                 <img src='/img/home-3.PNG' />
             </div>
         </Container>
-        
-
 
         <Container id='footer' style={{ background: 'black', border: '1px solid rgba(256, 256, 256, 0.2)', borderRadius: '10px', padding: '50px', position: 'relative', zIndex: '2' }}>
             <Row>
