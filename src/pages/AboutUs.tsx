1f7b3616f67c68a73def5944766962b850b4ca6a
Juampi Badino <juanpablobadino6@gmail.com>
2023-12-28 10:46:21 -0300

    About us - Mejoras (mobile)

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './AboutUs.css';
+
 import Container from 'react-bootstrap/Container';
 import { Row, Col } from 'react-bootstrap';
 
 const AboutUs = () => {
     return(
-        <Container style={{ background: '' }}>
+        <Container id='container-about-us' style={{ background: '' }}>
             <img id='navbar-laser' src='/img/navbar_laser.png' style={{ position: 'relative', zIndex: '1' }} />
             <div id='nav-bar' style={{ padding: '20px 30px'}}>
                 <div style={{ display: 'inline-block' }}>
@@ -21,36 +23,50 @@ const AboutUs = () => {
             </div>
             <img id='logo-inicio-reflejo' src='/img/Logo_Inicio_Reflejo.png' style={{ marginLeft: '40px', position: 'relative', zIndex: '1' }} />
 
-            <h1 style={{ background: '#171618', color: 'white', textAlign: 'center' }}  >
-                OUR MISSION
-            </h1>
+            <Container>
+                <h1 id='h1-about-us-our-mission' style={{ background: '#171618', color: 'white', textAlign: 'center' }}  >
+                    OUR MISSION
+                </h1>
 
-            <img src='/img/our-mission.jpg' />
+                <img src='/img/our-mission.jpg' style={{width: '100%'}} />
 
-            <p style={{ color: 'white' }}>
-                Our mission is to drive a leading immersive fan engagement platform, where sports, clubs, athletes and fans from around the world converge to forge deeper, more realistic relationships between all parties involved. We seek to build a space where the passion for sport translates into authentic and meaningful connections, promoting the interaction and active participation of all actors in the sporting world.
-            </p>
+                <p style={{ color: 'white' }}>
+                    Our mission is to drive a leading immersive fan engagement platform, where sports, clubs, athletes and fans from around the world converge to forge deeper, more realistic relationships between all parties involved. We seek to build a space where the passion for sport translates into authentic and meaningful connections, promoting the interaction and active participation of all actors in the sporting world.
+                </p>
 
-            <p style={{ color: 'white' }}>
-                We strive to foster an environment that allows fans to have unique and enriching experiences, while providing sports, clubs and athletes with the tools and technology necessary to strengthen their bond with their audience and take their engagement to the next level. Ultimately, our mission is to drive the evolution of relationships in the world of sport, bringing about a deeper and more authentic approach between all those involved.
-            </p>
+                <p style={{ color: 'white' }}>
+                    We strive to foster an environment that allows fans to have unique and enriching experiences, while providing sports, clubs and athletes with the tools and technology necessary to strengthen their bond with their audience and take their engagement to the next level. Ultimately, our mission is to drive the evolution of relationships in the world of sport, bringing about a deeper and more authentic approach between all those involved.
+                </p>
 
-            <img src='/img/laser-2.png' />
+                <div style={{position: 'relative'}}>
+                    <img src='/img/laser-2.png' style={{width: '100%'}} />
+                    <img src='/img/poligono_4.png' style={{width: '100%'}} />
+                    <img src='/img/panel_3_i.png' style={{width: '100%'}} />
+                    <img src='/img/panel_3_n_i.png' style={{transform: 'rotate(180deg)', width: '100%'}} />
+                    <img src='/img/panel_4_i.png' style={{width: '100%'}} />
+                    <div id='div-img-story-title'>
+                        <img src='/img/story-title.png' />
+                    </div>
+                    <div id='about-us-text-2'>
+                        <p style={{ color: 'white' }}>
+                            Barqia Sports was born as a result of the merger of an Argentine company, known as Barqia, and a Spanish one, Padel Fantasy. Our contact occurred thanks to participation in a reference event for startups and entrepreneurs at an international level. After several months of conversations, reflections and exchange of passions, we had the extraordinary idea of founding Barqia Sports, a comprehensive fan engagement platform.
+                        </p>
+                        <p style={{ color: 'white' }}>
+                            We share the same common interests and aspirations, focused on the application of cutting-edge and futuristic technology for the benefit of clubs, athletes and fans, with the purpose of creating a fully immersive platform with absolute connectivity between all the parties involved.
+                        </p>
+                    </div>
+                </div>
 
-            <img src='/img/poligono_4.png' />
+                <div style={{ textAlign: 'center'}}>
+                    <video autoPlay muted loop style={{width: '100%'}}>
+                        <source src="video/Alex_Promo_Juego.mp4" type="video/mp4" />
+                    </video>
+                </div>
 
-            <img src='/img/story-title.png' />
-
-            <p style={{ color: 'white' }}>
-                Barqia Sports was born as a result of the merger of an Argentine company, known as Barqia, and a Spanish one, Padel Fantasy. Our contact occurred thanks to participation in a reference event for startups and entrepreneurs at an international level. After several months of conversations, reflections and exchange of passions, we had the extraordinary idea of ​​founding Barqia Sports, a comprehensive fan engagement platform.
-            </p>
-            <p style={{ color: 'white' }}>
-                We share the same common interests and aspirations, focused on the application of cutting-edge and futuristic technology for the benefit of clubs, athletes and fans, with the purpose of creating a fully immersive platform with absolute connectivity between all the parties involved.
-            </p>
-
-            <h1>
-                ABOUT US
-            </h1>
+                <h1>
+                    ABOUT US
+                </h1>
+            </Container>
 
             <Container id='footer' style={{ background: 'black', border: '1px solid rgba(256, 256, 256, 0.2)', borderRadius: '10px', padding: '50px', position: 'relative', zIndex: '2' }}>
                 <Row>
